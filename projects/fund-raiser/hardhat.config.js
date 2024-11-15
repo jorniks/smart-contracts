ad2e504fc5c5a2dd567d1f15a8c64ee8f4588ea6
jorniks <dictforum@gmail.com>
2024-11-15 14:30:42 +0100

    Fix: removed network_rpc_url from .env and hard coded it in config file

--- projects/fund-raiser/hardhat.config.js
+++ projects/fund-raiser/hardhat.config.js
@@ -3,24 +3,24 @@ require('dotenv').config();
 require('@nomicfoundation/hardhat-ethers');
 require("@nomicfoundation/hardhat-verify");
 
-const { DEPLOYING_WALLET_PRIVATE_KEY, NETWORK_RPC_URL, ETHERSCAN_API_KEY } = process.env;
+const { DEPLOYING_WALLET_PRIVATE_KEY, LINEASCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.27",
   defaultNetwork: "lineaSepolia",
   sourcify: {
-    enabled: true
+    enabled: true,
   },
   networks: {
     hardhat: {},
     lineaSepolia: {
-      url: NETWORK_RPC_URL,
+      url: "https://rpc.sepolia.linea.build",
       chainId: 59141,
       accounts: [DEPLOYING_WALLET_PRIVATE_KEY],
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: LINEASCAN_API_KEY,
     customChains: [
       {
         network: "linea_sepolia",
